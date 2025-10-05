61679780af025a3e9be5f1a9f84ced566514509c
LourdesCabrera-2025 <00034023@uca.edu.sv>
2025-10-04 22:36:46 -0600

    :memo: Finalizando ejercicio 3 , agregando funciones de sugerencia y listando libros leidos y no leidos

--- 00034023_laboratorio5_seccion2/Ejercicio 3/script_ejercicio3.js
+++ 00034023_laboratorio5_seccion2/Ejercicio 3/script_ejercicio3.js
@@ -59,3 +59,62 @@ const nuevoObjeto = {
 }
 
 console.log(objetoCongelado, nuevoObjeto) */
+
+
+const Estanteria = {
+    libros: [{
+        nombre: 'EVERY THING IS FUCKED',
+        autor: 'Mark Manson',
+        leido: false
+    },
+    {
+        nombre: 'BAJO LA MISMA ESTRELLA',
+        autor: 'John Green',
+        leido: true
+    },
+    {
+        nombre: 'CAZADORES DE SOMBRAS 1 - CIUDAD DE HUESO',
+        autor: 'Cassandra Clare',
+        leido: true
+    },
+    {
+        nombre: 'BIBLIOTECARIA DE AUSCHWITZ',
+        autor: 'Antonio Iturbe',
+        leido: false
+    },
+    {
+        nombre: 'CATEDRAL DEL MAR',
+        autor: 'Ildefonso Falcones',
+        leido: false
+    },
+    {
+        nombre: 'LA LADRONA DE LIBROS',
+        autor: 'Markus Zusak',
+        leido: true
+    },
+    {
+        nombre: 'PSICOANALISTA',
+        autor: 'John Katzenbach',
+        leido: false
+    }],
+    log() {
+        const {libros } = this;
+        let resultado = ''
+        for(const libro of libros) {
+            const prefijo = libro.leido ? 'Ya has leido' : 'Aun no has leido' ;
+            resultado = `${resultado}
+            ${prefijo} = ${libro.nombre} de ${libro.autor}`
+        }
+        console.log(resultado)
+    },
+    sugerencia() {
+        const librosNoLeidos = this.libros.filter(libro => !libro.leido)
+        const indiceRandom = Math.floor(librosNoLeidos.length * Math.random())
+        const elementoRandom = librosNoLeidos[indiceRandom]
+        console.log(`Te sugiero  ${elementoRandom.nombre} de ${elementoRandom.autor}`)
+    }
+}
+
+console.log("Libros : ",  Estanteria.libros)
+Estanteria.log();
+Estanteria.sugerencia();
